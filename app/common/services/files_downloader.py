3b4400011aaebc8bbf6631cdaa3f6f75d4b06bae
emadmohamed95 <emadmohamed95@gmail.com>
2025-03-19 16:52:33 +0200

    chore: update dependencies and enhance session analysis
    - Upgrade Poetry version in poetry.lock to 2.1.1.
    - Refine package extras in poetry.lock to include platform-specific conditions.
    - Modify `pyproject.toml` to support Python versions up to 3.13.2.
    - Implement new functionality in `service.py` for decompressing and saving JSON files from S3.
    - Add `RRWebSessionUtils` class in `rrweb.py` for handling session data and converting events to video.
    - Enhance `FilesDownloader` class to include a method for retrieving the local temporary directory.

--- app/common/services/files_downloader.py
+++ app/common/services/files_downloader.py
@@ -1,9 +1,12 @@
-import tempfile
-import shutil
 import os
+import shutil
+import tempfile
+from typing import Any, Optional
+
 import boto3
 from common.services.logger import logger
 from settings import settings
+
 
 class FilesDownloader:
     def __init__(self, s3_client: boto3.client):
@@ -12,17 +15,23 @@ class FilesDownloader:
         self.temp_dir = tempfile.mkdtemp()
         logger.info(f"Temporary folder created: {self.temp_dir}")
 
-
     def download_file_from_s3(self, key: str) -> str:
         local_file_path = os.path.join(self.temp_dir, os.path.basename(key))
         self.s3_client.download_file(self.bucket_name, key, local_file_path)
         logger.info(f"Downloaded {key} to {local_file_path}")
         return local_file_path
 
+    def get_local_temp_dir(self) -> str:
+        return self.temp_dir
 
-    def __enter__(self):
+    def __enter__(self) -> "FilesDownloader":
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(
+        self,
+        exc_type: Optional[type],
+        exc_value: Optional[Exception],
+        traceback: Optional[Any],
+    ) -> None:
         shutil.rmtree(self.temp_dir)
         logger.info(f"Temporary folder deleted: {self.temp_dir}")
